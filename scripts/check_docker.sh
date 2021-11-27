89cad5224d7c0ec35f10cd8856d83c003976c774
LordAlain <akommi1997@gmail.com>
2021-11-27 15:39:16 -0500

    move imgs

---
+++ scripts/check_docker.sh
@@ -0,0 +1,15 @@
+
+
+docker --version
+# docker pull
+sudo docker pull nvcr.io/nvidia/l4t-ml:r32.6.1-py3
+docker image ls
+
+
+docker exec -it <CONTAINER_ID_OR_NAME> bash
+
+
+# Docker Run commands
+# sudo docker run -it --rm --runtime nvidia --network host nvcr.io/nvidia/l4t-ml:r32.6.1-py3
+
+sudo docker run -it --rm --runtime nvidia --network host -v /home/user/project:/location/in/container nvcr.io/nvidia/l4t-ml:r32.6.1-py3
