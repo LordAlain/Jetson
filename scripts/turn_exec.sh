a0667d4d634ac7739bdfe329b19f90f5790afc9b
LordAlain <akommi1997@gmail.com>
2021-11-29 14:44:57 -0500

    Create turn_exec.sh

---
+++ scripts/turn_exec.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+
+chmod u+x ./check_docker.sh ./docker_base.sh ./docker_build_ml.sh ./docker_build.sh ./docker_run.sh ./docker_push.sh ./docker_test_ml.sh ./l4t_version.sh ./turn_exec.sh ./install_libraries.sh
